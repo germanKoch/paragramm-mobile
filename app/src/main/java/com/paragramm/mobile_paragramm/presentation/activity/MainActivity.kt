667bed7433139e0e9a8ddb73d7e9bec9387df562
german <kochgerm@gmail.com>
2022-05-14 12:23:58 +0300

    back button added

--- app/src/main/java/com/paragramm/mobile_paragramm/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/paragramm/mobile_paragramm/presentation/activity/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.paragramm.mobile_paragramm.presentation.activity
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import androidx.paging.ExperimentalPagingApi
 import com.paragramm.mobile_paragramm.R
 import com.paragramm.mobile_paragramm.presentation.fragments.ConversationsFragment
-
+import com.paragramm.mobile_paragramm.presentation.fragments.MessagesFragment
 
 @ExperimentalPagingApi
 class MainActivity : AppCompatActivity() {
@@ -19,6 +20,20 @@ class MainActivity : AppCompatActivity() {
 
         supportFragmentManager.commit {
             replace<ConversationsFragment>(R.id.fragment_container_view)
+        }
+        //TODO: implement in better way
+        findViewById<View>(R.id.back_button).setOnClickListener {
+            when (supportFragmentManager.findFragmentById(R.id.fragment_container_view)) {
+                is MessagesFragment -> {
+                    supportFragmentManager.commit {
+                        replace<ConversationsFragment>(R.id.fragment_container_view)
+                    }
+                }
+
+                is ConversationsFragment -> {
+                    //ignore
+                }
+            }
         }
     }
 
